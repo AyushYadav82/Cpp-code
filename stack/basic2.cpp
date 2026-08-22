34367781fb743eab48f39dbab705ea17cf3be326
AyushYadav82 <ayushyadav190206@gmail.com>
2026-08-22 11:54:14 +0530

    stack

---
+++ stack/basic2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+int main(){
+    stack<int> s;
+    s.push(10);
+    s.push(20);
+    while(!s.empty()){
+        cout<<s.top()<<" ";
+        s.pop();
+    }
+    return 0;
+}
